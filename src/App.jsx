1a261af1160fdec71866b2b140557765949bad1d
WEIzone082 <wtfuhell@gmail.com>
2022-03-24 11:46:22 +0800

    1.0

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+// sass loader:npm install node-sass
+import "./assets/basics.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <p>app</p>
+    </div>
+  );
+}
+
+export default App;
