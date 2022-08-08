656634ae87a7388f545883fb697d670d60321af6
WEIzone082 <wtfuhell@gmail.com>
2022-08-08 11:50:42 +0800

    actions

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,27 +2,27 @@
 // https://github.com/academind/react-complete-guide-code/blob/19-advanced-redux/code/06-finished/src/store/cart-actions.js
 
 export const fetchCart = () => {
-  return (dispatch) => {
-    const fetchData = async () => {
-      const response = await fetch(
-        "https://react-basics-faa3f-default-rtdb.firebaseio.com/"
-      );
+    return (dispatch) => {
+        const fetchData = async () => {
+            const response = await fetch(
+                "https://react-basics-faa3f-default-rtdb.firebaseio.com/"
+            );
 
-      if (!response.ok) {
-        throw new Error("cannot fetch");
-      }
+            if (!response.ok) {
+                throw new Error("cannot fetch");
+            }
 
-      const data = await response.json();
+            const data = await response.json();
 
-      return data;
+            return data;
+        };
+
+        try {
+            const data = await fetchData()
+            // dispatch ....
+            // dispatch functions here
+        } catch (error) {
+            console.log("error");
+        }
     };
-
-    try{
-        const data = await fetchData()
-        // dispatch ....
-        // dispatch functions here
-    }catch(error){
-        console.log("error");
-    }
-  };
 };
